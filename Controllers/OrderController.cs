d811609a6b0b5bbadf16b0f34018806f52e12efe
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-11-12 16:39:14 -0300

    Remove documentation for now (I don't have time for that anyways)

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -14,29 +14,16 @@ using RabbitMQ.Client;
 
 namespace Iida.Api.Controllers;
 
-/// <summary>
-/// Controlador para la gestión de órdenes de cálculo de evapotranspiración.
-/// </summary>
 [AllowAnonymous, ApiController, EnableCors("AllowAll"), Route("api/crop-order")]
 public class OrderController : ControllerBase {
 	private readonly ILogger _logger;
 	private readonly Parameters _parameters;
 
-	/// <summary>
-	/// Constructor del controlador.
-	/// </summary>
-	/// <param name="logger">Parámetro de inyección del logger del programa.</param>
-	/// <param name="parameters">Parámetro de inyección de parámetros del programa.</param>
 	public OrderController(ILogger<OrderController> logger, Parameters parameters) {
 		_logger = logger;
 		_parameters = parameters;
 	}
 	
-	/// <summary>
-	/// Crea una orden.
-	/// </summary>
-	/// <param name="request">JSON order request.</param>
-	/// <returns>HTTP status code.</returns>
 	[HttpPost("place-order")]
 	public async Task<ActionResult<Order>> PlaceRequest([FromBody] Order request) {
 		var factory = new ConnectionFactory() { HostName = _parameters.RabbitMqHost };
